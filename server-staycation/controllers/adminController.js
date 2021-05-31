50ae40ff6b06b2e3a01234c42e95bcbeade88a5d
yohanes123 <yohanespratitis@gmail.com>
2021-05-31 15:01:40 +0700

    Create Category

--- server-staycation/controllers/adminController.js
+++ server-staycation/controllers/adminController.js
@@ -1,9 +1,17 @@
+const Category = require('../models/Category');
+
 module.exports = {
     viewDashboard : (req, res) => {
         res.render('admin/dashboard/view_dashboard');
     },
     viewCategory : (req, res) => {
         res.render('admin/category/view_category');
+    },
+    addCategory : async (req,res) => {
+        const { name } = req.body;
+        // console.log(name);
+        await Category.create({ name });
+        res.redirect('/admin/category');
     },
     viewBank : (req, res) => {
         res.render('admin/bank/view_bank');
