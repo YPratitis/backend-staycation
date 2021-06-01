34f9b83d344339cf44bcb9f9aaed2158a8fd60cd
yohanes123 <yohanespratitis@gmail.com>
2021-06-01 14:12:38 +0700

    Alert Message &Title Dinamis, & install express session dan connect-flash

--- server-staycation/app.js
+++ server-staycation/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // override untuk update 
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 // import mongoose
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/db_mern', 
@@ -25,6 +27,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie:  { maxAge: 60000 }
+}));
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
